f66ad572df899eeeabe9b3a22925b4d1f8b32f7a
roland.pakai <roland.pakai@lightware.com>
2023-07-05 09:51:00 +0200

    Create Navbar and Header

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,31 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className='text-blue-400 text-7xl'>Hello</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className='max-w-screen-2xl m-auto bg-white'>
+        {/* NAVBAR */}
+        <nav className='bg-white p-2 flex justify-between'>
+          <a href="" className='font-bold text-gray-700 text-2xl'>
+            OpenTable
+          </a>
+          <div>
+            <div className='flex'>
+              <button className='bg-blue-400 text-white border p-1 px-4 mr-3 rounded'>Sign in</button>
+              <button className='text-gray-700 border p-1 px-4 rounded'>Sign up</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        <main>
+           {/* HEADER */}
+          <div className='h-64 p-2 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]'>
+            <div className='text-center mt-10'>
+              <h1 className='text-white text-5xl font-bold mb-2'>Find your table for any occasion</h1>
+            </div>
+          </div>
+           {/* HEADER */}
+        </main>
+      </main>
     </main>
   )
 }
