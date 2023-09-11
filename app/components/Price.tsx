a208e7c83bce360f735afc09461adaf7079513b7
roland.pakai <roland.pakai@lightware.com>
2023-09-11 19:08:04 +0200

    Rewrite price render logic

--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,13 +1,15 @@
 import { PRICE } from '@prisma/client'
 import React from 'react'
 
+const PRICE_SIGN_COUNT = 4;
+
 interface Props {
   price: PRICE
 }
 
 const repeatChar = (n: number) => {
   if (n <= 0) {
-    return '????';
+    return '?'.repeat(n - PRICE_SIGN_COUNT);
   }
   
   return '$'.repeat(n);
@@ -16,52 +18,46 @@ const repeatChar = (n: number) => {
 const renderPriceIndicator = (value: number) => {
   return (
     <>
-      <span>repeatChar(value)</span>
+      <span>{repeatChar(value)}</span>
       {
-        (4 - value) > 0 &&  
-        <span className='text-gray-400'>repeatChar(4 - value)</span>
+        (PRICE_SIGN_COUNT - value) > 0 
+        &&  
+        <span className='text-gray-400'>
+          {repeatChar(4 - value)}
+        </span>
       }
-      
     </>
   )
 }
 
 export default function Price({price}: Props) {
-  
   const renderPrice = () => {
     switch (price) {
-      case PRICE.CHEAP :      
-        return (
-          <>
-            <span>$$</span>
-            <span className='text-gray-400'>$$</span>
-          </>
-        )
+      case PRICE.CHEAP:      
+        return renderPriceIndicator(1)
 
       case PRICE.REGULAR: 
-        return (
-          <>
-            <span>$$$</span>
-            <span className='text-gray-400'>$</span>
-          </>
-        )
+        return renderPriceIndicator(2)
 
       case PRICE.EXPENSIVE:
-        return (
-          <span>$$$$</span>
-        )
+        return renderPriceIndicator(4)
 
-      default:  return (
-        <span>????</span>
-      ) 
+      default:  
+        return renderPriceIndicator(0)
     }
   }
+
+  const capitalizeFirstLetter = (word: string): string => {
+    if (!word) {
+      return '?';
+    }
+  
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  };
   
   return (
-    <p className='flex mr-3'>
-      {
-        renderPrice()
-      }
+    <p className='flex mr-3' title={capitalizeFirstLetter(price)}>
+      {renderPrice()}
     </p>
   )
 }
