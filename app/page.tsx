3927dc0b8af267ebd0ebd812dcda7a8f50172808
roland.pakai <roland.pakai@lightware.com>
2023-09-11 18:52:34 +0200

    Create fetchRestaurants function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number,
+  name: string,
+  main_image: string,
+  cuisine: Cuisine,
+  location: Location,
+  price: PRICE,
+  slug: string,
+}
 
+const prisma = new PrismaClient();
+
+const fetchRestaurants =async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true,
+    }
+  });
+
+  return restaurants;
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
   return (
     <main>
       <Header />
