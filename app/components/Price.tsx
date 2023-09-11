58f6862f19b99285766a58456a573b9677e809ba
roland.pakai <roland.pakai@lightware.com>
2023-09-11 19:22:19 +0200

    Simplify price render logic

--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,21 +1,23 @@
 import { PRICE } from '@prisma/client'
 import React from 'react'
 
-const PRICE_SIGN_COUNT = 4;
-
 interface Props {
   price: PRICE
 }
 
+const PriceValue = {
+  CHEAP: 1,
+  REGULAR: 2,
+  EXPENSIVE: 4,
+}
+
+const PRICE_SIGN_COUNT = PriceValue.EXPENSIVE;
+
 const repeatChar = (n: number) => {
-  if (n <= 0) {
-    return '?'.repeat(n - PRICE_SIGN_COUNT);
-  }
-  
   return '$'.repeat(n);
 }
 
-const renderPriceIndicator = (value: number) => {
+const renderPrice = (value: number) => {
   return (
     <>
       <span>{repeatChar(value)}</span>
@@ -23,41 +25,22 @@ const renderPriceIndicator = (value: number) => {
         (PRICE_SIGN_COUNT - value) > 0 
         &&  
         <span className='text-gray-400'>
-          {repeatChar(4 - value)}
+          {repeatChar(PRICE_SIGN_COUNT - value)}
         </span>
       }
     </>
   )
 }
 
+const capitalizeFirstLetter = (word: string): string => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+};
+
 export default function Price({price}: Props) {
-  const renderPrice = () => {
-    switch (price) {
-      case PRICE.CHEAP:      
-        return renderPriceIndicator(1)
 
-      case PRICE.REGULAR: 
-        return renderPriceIndicator(2)
-
-      case PRICE.EXPENSIVE:
-        return renderPriceIndicator(4)
-
-      default:  
-        return renderPriceIndicator(0)
-    }
-  }
-
-  const capitalizeFirstLetter = (word: string): string => {
-    if (!word) {
-      return '?';
-    }
-  
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  };
-  
   return (
     <p className='flex mr-3' title={capitalizeFirstLetter(price)}>
-      {renderPrice()}
+      {renderPrice(PriceValue[price])}
     </p>
   )
 }
