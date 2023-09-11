d21e261f514813880314cbb2d42df61574441aa6
roland.pakai <roland.pakai@lightware.com>
2023-09-11 18:09:42 +0200

    Set Home as server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-"use client"
-
-import { Inter } from '@next/font/google'
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
