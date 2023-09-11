6f3b9d2ba515d08587168daa601941ef1d5100d4
roland.pakai <roland.pakai@lightware.com>
2023-09-11 14:05:08 +0200

    Extract nestead RestaurantLayout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCard from './components/ReservationCard';
@@ -7,22 +6,19 @@ import RestaurantNavBar from './components/RestaurantNavBar';
 import Reviews from './components/Reviews';
 import Title from './components/Title';
 
-export default function Restaurant() {
+export default function RestaurantDetails() {
   return (
     <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[70%] rounded p-3 shadow'>
-          <RestaurantNavBar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className='w-[27%] relative text-reg'>
-          <ReservationCard />
-        </div>
+      <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className='w-[27%] relative text-reg'>
+        <ReservationCard />
       </div>
     </>
   )
