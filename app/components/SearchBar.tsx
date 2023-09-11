3dc3a3f362fea75b13d5babccd43d69db7d4f784
roland.pakai <roland.pakai@lightware.com>
2023-09-11 11:07:55 +0200

    Create SearchBar component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+export default function SearchBar() {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
+
+  const searchButtonClickHandler = () => {
+    if (location === 'banana') 
+      return;
+
+    router.push('/search');
+  }
+  
+  return (
+    <div className='text-left text-lg py-3 m-auto flex justify-center'>
+    <input 
+      className='rounded text-lg mr-3 p-2 w-[450px]' 
+      type='text' 
+      placeholder='Location, Restaurant, or Cuisine'
+      value={location}
+      onChange={(e) => setLocation(e.target.value)}
+    />
+    <button 
+      className='rounded bg-red-600 px-9 py-2 text-white'
+      onClick={searchButtonClickHandler}
+      >
+        Let's go
+      </button>
+  </div>
+  )
+}
