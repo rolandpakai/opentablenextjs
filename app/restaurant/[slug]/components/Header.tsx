6b6db75dd713b7aa560d52679f07e388cbfcdfc3
roland.pakai <roland.pakai@lightware.com>
2023-09-12 13:21:33 +0200

    Set Header title

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,10 +1,17 @@
 
-export default function Header(){
+const renderTitle = (name: string): string => {
+  const nameArray = name.split("-");
+  nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+
+  return nameArray.join(" ");
+}
+
+export default function Header({name}: {name: string}){
   return (
     <div className='h-96 overflow-hidden'>
     <div className='bg-center h-full flex justify-center items-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984]'>
       <h1 className='text-7xl text-white capitalize text-center'>
-        DNB Budapest – Budapest Marriott Hotel
+        {renderTitle(name)}
       </h1>
     </div>
   </div>
