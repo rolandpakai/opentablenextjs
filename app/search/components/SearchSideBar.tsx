333e309b3943257da9ad541c8fed6419ce5a5f26
roland.pakai <roland.pakai@lightware.com>
2023-09-23 19:23:34 +0200

    Build up SearchSideBar dynamically

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,41 +1,111 @@
+import { PRICE } from "@prisma/client";
+import Link from "next/link";
+import { SearchParamsType } from "../page";
 
-export default function SearchSideBar(){
+type ListLike = {
+  id: number,
+  name: string,
+};
+
+const priceOptions = [{
+  name: "price",
+  label: "$",
+  price: PRICE.CHEAP,
+
+},{
+  name: "price",
+  label: "$$",
+  price: PRICE.REGULAR,
+},{
+  name: "price",
+  label: "$$$",
+  price: PRICE.EXPENSIVE,
+}];
+
+const getSearchHRef = (paramName: string, paramValue: string, searchParams: SearchParamsType) => {
+  return {
+    pathname: "/search",
+    query: {
+      ...searchParams,
+      [paramName]: paramValue,
+    }
+  }
+};
+
+const getRoundedCssClass = (length: number, index: number) => {
+  let className = "";
+
+  switch (true) {
+    case index === 0: className = "rounded-l"; break;
+    case index === length - 1: className = "rounded-r"; break;
+    default: className = "";
+  }
+
+  return className;
+};
+
+const renderList =<T extends ListLike>(
+  type: string, 
+  list: T[],
+  searchParams: SearchParamsType,
+  ) =>{
+  return list.map(item => (
+    <Link 
+      key={item.id} 
+      href={getSearchHRef(type, item.name, searchParams)}
+      className="font-light text-reg capitalize"
+    >
+      {item.name}
+    </Link>
+  ));
+}
+
+export default function SearchSideBar({
+    locations, 
+    cuisines,
+    searchParams,
+  }: {
+    locations: ListLike[], 
+    cuisines: ListLike[],
+    searchParams: SearchParamsType,
+}) {
+
+  const filterOptions = [{
+    name: "city",
+    options: locations,
+  }, {
+    name: "cuisine",
+    options: cuisines,
+  }];
 
   return (
     <div className="w-1/5">
-    <div className="border-b pb-4">
-      <h1 className="mb-2">Region</h1>
-      <p className="font-light text-reg">Toronto</p>
-      <p className="font-light text-reg">Ottawa</p>
-      <p className="font-light text-reg">Montreal</p>
-      <p className="font-light text-reg">Hamilton</p>
-      <p className="font-light text-reg">Kingston</p>
-      <p className="font-light text-reg">Niagara</p>
-    </div>
-    <div className="border-b pb-4 mt-3">
-      <h1 className="mb-2">Cuisine</h1>
-      <p className="font-light text-reg">Mexican</p>
-      <p className="font-light text-reg">Italian</p>
-      <p className="font-light text-reg">Chinese</p>
-    </div>
-    <div className="mt-3 pb-4">
-      <h1 className="mb-2">Price</h1>
-      <div className="flex">
-        <button className="border w-full text-reg font-light rounded-l p-2">
-          $
-        </button>
-        <button
-          className="border-r border-t border-b w-full text-reg font-light p-2"
-        >
-          $$
-        </button>
-        <button
-          className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r"
-        >
-          $$$
-        </button>
+      {
+        filterOptions.map(({name, options}) => (
+          <div key={name} className="border-b pb-4 flex flex-col">
+            <h1 className="mb-2 capitalize">{name}</h1>
+              {renderList(name, options, searchParams)}
+          </div>
+        ))
+      }
+
+      <div className="mt-3 pb-4">
+        <h1 className="mb-2">Price</h1>
+        <div className="flex">
+          {
+            priceOptions.map(({name, label, price}, index) => { 
+              return (
+                <Link 
+                  key={label}
+                  href={getSearchHRef(name, price, searchParams)}
+                  className={`border w-full text-reg font-light p-2 ${getRoundedCssClass(priceOptions.length, index)}`}
+                >
+                  {label}
+                </Link>
+            )})
+          }
+        </div>
       </div>
     </div>
-  </div>
   )
 }
