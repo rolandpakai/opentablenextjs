cb3b7d4fa7fa4e5b4c840ef8aabd284818798bba
roland.pakai <roland.pakai@lightware.com>
2023-10-28 16:50:08 +0200

    Implement signin, signup, fetchUser functions

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,22 +1,107 @@
 import axios from "axios";
+import { getCookie } from "cookies-next";
+import { useContext } from "react";
+import { AuthenticationContext } from "../app/context/AuthContext";
 
 const useAuth = () => {
-  const signin = async ({email, password}: {email: string, password: string}) => {
+  const { data, error, loading, setAuthState } = useContext(AuthenticationContext);
+
+  const signin = async (
+    {email, password}: {email: string, password: string},
+    handleClose: () => void
+  ) => {
+    setAuthState({ data: null, error: null, loading: true });
+    
     try {
-      const response = await axios.post("http://localhost:3000/api/auth/signin", {
+      const response = await axios.post("http://localhost:3000/api/auth/signin", 
+      {
         email,
         password
-      })
-    } catch (err) {
+      });
 
+      setAuthState({ data: response.data, error: null, loading: false });
+      handleClose();
+    } catch (error: any) {
+      setAuthState({ data: null, error: error.response.data.errorMessage, loading: false });
     }
   }
 
-  const signup = async () => {}
+  const signup = async (
+    {
+      email, 
+      password,
+      firstName,
+      lastName,
+      city,
+      phone,
+    }: {
+      email: string, 
+      password: string,
+      firstName: string,
+      lastName: string,
+      city: string,
+      phone: string,
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({ data: null, error: null, loading: true });
+  
+    try {
+      const response = await axios.post("http://localhost:3000/api/auth/signup", 
+      {
+        email,
+        password,
+        firstName,
+        lastName,
+        city,
+        phone,
+      });
+
+      setAuthState({ data: response.data, error: null, loading: false });
+      handleClose();
+    } catch (error: any) {
+      setAuthState({ data: null, error: error.response.data.errorMessage, loading: false });
+    }
+  }
+
+  const fetchUser = async () => {
+    try {
+      const jwt = getCookie("jwt");
+
+      if (!jwt) {
+        return setAuthState({ 
+          data: null, 
+          error: null, 
+          loading: false,
+        });
+      }
+
+      const response = await axios.get("http://localhost:3000/api/me", {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      });
+
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({ 
+        data: response.data, 
+        error: null, 
+        loading: false,
+      });
+    } catch (error: any) {
+      setAuthState({ 
+        data: null, 
+        error: error.response.data.errorMessage, 
+        loading: false,
+      });
+    }
+  }
 
   return {
     signin,
-    signup
+    signup,
+    fetchUser,
   }
 }
 
