8ebbc53df2963987aa81464dbb3e6afb2c76871f
roland.pakai <roland.pakai@lightware.com>
2023-10-28 17:10:51 +0200

    Add loading check and Logout button

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from 'next/link';
 import AuthModal from './AuthModal';
+import { useContext } from 'react';
+import { AuthenticationContext } from '../context/AuthContext';
 
 export default function NavBar() {
+  const { data, loading } = useContext(AuthenticationContext);
 
   return (
     <nav className='bg-white p-2 flex justify-between'>
@@ -9,10 +14,20 @@ export default function NavBar() {
       OpenTable{" "}
     </Link>
     <div>
-      <div className='flex'>
-        <AuthModal isSignIn={true} />
-        <AuthModal isSignIn={false} />
-      </div>
+      {loading ? null : (
+        <div className='flex'>
+          {data ? (
+            <button className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'>
+              Logout
+            </button>
+          ) : (
+            <>
+              <AuthModal isSignIn={true} />
+              <AuthModal isSignIn={false} />
+            </>
+          )}
+        </div>
+      )}
     </div>
   </nav>
   )
