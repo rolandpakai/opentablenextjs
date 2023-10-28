627c75b615583dae96501823e5bbfcd6574e2e10
roland.pakai <roland.pakai@lightware.com>
2023-10-28 17:01:51 +0200

    Fix return value

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -30,7 +30,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       city: true,
       phone: true,
     }
-  })
+  });
 
-  return res.json({ user })
+  if (!user) {
+    return res.status(401).json({ 
+      errorMessage: "User not found" 
+    });
+  }
+
+  return res.json({ 
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    phone: user.phone,
+    city: user.city,
+   })
 }
