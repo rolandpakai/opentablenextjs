a59c381f8e80cb8161065799d6d241b4ff007ef9
roland.pakai <roland.pakai@lightware.com>
2023-10-30 09:38:16 +0100

    Create availability.ts

---
+++ pages/api/restaurant/[slug]/availability.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { times } from "../../../../data";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { 
+  const {slug, day, time, partySize} = req.query as {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: string;
+  };
+
+  if (!day || !time || !partySize) {
+    return res.status(400).json({
+      errorMessage: "Invalid data provided"
+    });
+  }
+
+  const searchTimes = times.find(t => {
+    return t.time === time
+  })?.searchTimes;
+
+  if (!searchTimes) {
+    return res.status(400).json({
+      errorMessage: "Invalid data provided"
+    });
+  }
+
+  return res.json({ searchTimes });
+}
