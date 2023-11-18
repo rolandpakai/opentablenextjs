fd34d43dc38c0a06963776bd9029977118ad6161
roland.pakai <roland.pakai@lightware.com>
2023-11-18 15:31:26 +0100

    Add slug prop to ReservationCard

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -57,7 +57,11 @@ export default async function RestaurantDetails({ params }: {params: {slug: stri
         <Reviews reviews={restaurant.reviews} />
       </div>
       <div className='w-[27%] relative text-reg'>
-        <ReservationCard openTime={restaurant.open_time} closeTime={restaurant.close_time} />
+        <ReservationCard 
+          openTime={restaurant.open_time} 
+          closeTime={restaurant.close_time} 
+          slug={restaurant.slug}
+        />
       </div>
     </>
   )
